8622a1997fc80eef6547776004e4c71048b36568
Thomas VB <van.bouwel.thomas@gmail.com>
2016-07-09 11:21:11 +0100

    Current State of repo

---
+++ Assets/#project/Scripts/UI/MenuCheckbox.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuCheckbox: MenuButton {
+	
+	public UnityEvent _ClickOn;
+	public UnityEvent _ClickOff;
+
+	protected bool _IsSelected = false;
+	public Color _NeutralColor = new Color(255f,255f,255f,55f);
+	public Color _SelectedColor = new Color(50f,130f,255f,55f);
+		
+	public void HoverOn(WandController C){
+		base.HoverOn(C);
+		if (C.triggerDown)
+		{
+			if (_IsSelected)
+			{
+				Deselect ();
+			} else
+			{
+				Select();
+			}
+		}
+	}
+		
+	public void Deselect(){
+		_IsSelected = false;
+		_Background.color = _NeutralColor;
+		_ClickOff.Invoke ();
+	}
+
+	public void Select(){
+		_IsSelected = true;
+		_Background.color = _SelectedColor;
+		_ClickOn.Invoke ();
+	}
+}
--- Assets/#project/Scripts/Util/CopyRotation.cs
+++ Assets/#project/Scripts/Util/CopyRotation.cs
@@ -4,9 +4,20 @@ using System.Collections;
 public class CopyRotation : MonoBehaviour {
 
 	public Transform _Target;
+	public Quaternion _Startrotation;
+	public bool _MaintainRelativeRoation = false;
+
+	void Start(){
+
+	}
 
 	// Update is called once per frame
 	void Update () {
-		transform.rotation = _Target.rotation;
+		if (_MaintainRelativeRoation)
+		{
+		} else
+		{
+			transform.rotation = _Target.rotation;
+		}
 	}
 }
