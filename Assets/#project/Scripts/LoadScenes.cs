2a4c59ddfc3f0999415c64bc5d258dad4fe23555
Thomas VB <van.bouwel.thomas@gmail.com>
2016-07-12 20:49:54 +0100

    Update Roomscale to my Brussels Appartment

---
+++ Assets/#project/Scripts/LoadScenes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScenes : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (InputController.controllers [0].menuDown)
+		{
+			Application.LoadLevel("Main");
+		}
+
+		if (InputController.controllers[1].menuDown)
+		{
+			Application.LoadLevel("Model");
+		}
+	}
+}
