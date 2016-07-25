2c82df43a469e16df93638b1cf4c286acc195e62
Thomas VB <van.bouwel.thomas@gmail.com>
2016-07-25 14:36:04 +0200

    Reposition playarea at start to place player in front of the table.

---
+++ Assets/#project/Scripts/Util/PlayAreaPositioner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayAreaPositioner : MonoBehaviour {
+
+	public Transform _PlayArea;
+	public Transform _Camera;
+	public Transform _PlayerStartAnchor;
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("ResetPlayAreaPosition", 0.1f);
+	}
+
+	public void ResetPlayAreaPosition(){
+		Vector3 positionCorrection = _PlayerStartAnchor.position - _Camera.position;
+		positionCorrection = new Vector3 (positionCorrection.x, 0, positionCorrection.z);
+		_PlayArea.Translate(positionCorrection);
+	}
+}
