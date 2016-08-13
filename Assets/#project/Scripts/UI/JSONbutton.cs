c674f9cb9d685dc79a3c2fb843bdae924676e43d
Thomas VB <van.bouwel.thomas@gmail.com>
2016-08-13 14:08:36 +0200

    menu

---
+++ Assets/#project/Scripts/UI/JSONbutton.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JSONbutton : MonoBehaviour {
+
+    public string JsonFileLocation = null;
+
+}
---
+++ Assets/#project/Scripts/UI/JSONbuttonSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JSONbuttonSpawner : MonoBehaviour {
+
+    public GameObject _ImagePrefab;
+    public Transform _Parent;
+
+    public string _FilePath;
+
+    // Use this for initialization
+    void Start () {
+
+        _FilePath = Application.dataPath + _FilePath;
+
+        //create directory if not exists
+        if (!System.IO.Directory.Exists(_FilePath))
+            System.IO.Directory.CreateDirectory(_FilePath);
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
