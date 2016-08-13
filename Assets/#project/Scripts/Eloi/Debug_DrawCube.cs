cc553340c8e93d234040d29859ed999fdf022b4b
Jams Center <Jams.Center@gmail.com>
2016-08-13 14:21:35 +0200

    It is commiiiinngngngngn !

--- Assets/#project/Scripts/Eloi/Debug_DrawCube.cs
+++ Assets/#project/Scripts/Eloi/Debug_DrawCube.cs
@@ -23,8 +23,12 @@ public class Debug_DrawCube : MonoBehaviour {
                 {
                     GameObject objCreated = GameObject.Instantiate(_prefabPointPath, state._lookState._rootPosition, Quaternion.identity) as GameObject;
                     _createdCube.Add(objCreated);
+                    objCreated.GetComponent<Renderer>().material.SetColor("_MainTex", Color.green);
+
                     objCreated = GameObject.Instantiate(_prefabPointLook, state._lookState._lookAtPosition, Quaternion.identity) as GameObject;
                     _createdCube.Add(objCreated);
+                    objCreated.GetComponent<Renderer>().material.SetColor("_MainTex", Color.cyan);
+
                     //Debug.DrawLine(state._lookState._rootPosition, state._lookState._lookAtPosition, Color.grey, 100);
                 }
             }
